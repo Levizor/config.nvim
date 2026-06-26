222667a039f4f0aa841db1d82152dc17ce551d5b
Levizor <levizor@disroot.org>
2026-06-26 17:29:21 +0200

    keybind

--- lua/plugins/treesitter-manager.lua
+++ lua/plugins/treesitter-manager.lua
@@ -5,6 +5,9 @@ return {
     opts = {
       auto_install = true,
     },
+    keys = {
+      { "<leader>tm", "<cmd>TSManager<cr>", {desc = "TreeSitter manager"} }
+    },
     config = function(_, opts)
       require("tree-sitter-manager").setup(opts)
     end,
